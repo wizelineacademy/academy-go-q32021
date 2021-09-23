942b89157407e3c45d7b4fdb4d93bd614e9d80fe
marcxz <marcxz@gmail.com>
2021-09-23 15:26:41 -0500

    feat (infraestructure): Add csv infraestructure layer and an example file

---
+++ infraestructure/csv/csv.go
@@ -0,0 +1,18 @@
+package infraestructure
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+// ReadCSVFile - Read a CSV file with a filename specified
+func ReadCSVFile(f string) ([]byte, error) {
+	p := fmt.Sprintf("./files/%s", f)
+	l, err := ioutil.ReadFile(p)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return l, nil
+}
